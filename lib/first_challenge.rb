f2b9fa33c30edca02306bbca4733049051f4ac5e
Ashleigh Chun <ashleigh.chun@wework.com>
2018-11-04 04:25:15 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -8,8 +8,8 @@ def first_challenge
     },
     :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {},
-      :heroine => {},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => []
     }
    }
